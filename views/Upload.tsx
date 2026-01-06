3d4e1a0eaa9bf8ebe5129740f0e501f392cea027
portalguitarshred <portalguitarshred@gmail.com>
2026-01-06 00:39:45 -0300

    Update Upload.tsx

--- views/Upload.tsx
+++ views/Upload.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Youtube, CheckCircle2, Link as LinkIcon, Play, Loader2, Brain, Activity, ShieldCheck, Zap, Sword, Radar } from 'lucide-react';
